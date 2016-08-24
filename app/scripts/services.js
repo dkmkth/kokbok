f82cc8759e630a016e8c6bc0b5327efa68f3ac88
Emil Lindblom <emillind@kth.se>
2016-08-24 23:37:59 +0200

    services.js cleanup.

--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,73 +2,6 @@ angular.module('cookbook.services', [])
 
 
 .factory('recipeFactory', function($http) {
-  var recipes = [
-    {"Namn": "Recept 1",
-    "Ingredients": [
-      {"Namn": "Mjöl", "Mängd": "2 dl"},
-      {"Namn": "Vatten", "Mängd": "2 ml"},
-    ],
-    "Description": "Rör om",
-    "Tags": ["asd", "aa", "p"],
-    "Comments": [
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Paul", "Comment": "absolut inte"}, {"Poster": "Paul", "Comment": "absolut inte"}]},
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Paul", "Comment": "absolut inte"}, {"Poster": "Paul", "Comment": "absolut inte"}]}
-    ]},
-    {"Namn": "Recept 2",
-    "Ingredients": [
-      {"Namn": "Botten", "Mängd": "670 dl"},
-      {"Namn": "Topping", "Mängd": "2 liter"},
-    ],
-    "Description": "Lägg på Topping",
-    "Tags": ["asd", "aa", "p"],
-    "Comments": [
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Malle", "Comment": "Fuck you"},]}
-    ]},
-    {"Namn": "Ägg",
-    "Ingredients": [
-      {"Namn": "Äggvita", "Mängd": "670 dl"},
-      {"Namn": "Äggula", "Mängd": "2 liter"},
-      {"Namn": "Skal", "Mängd": "78 liter"},
-    ],
-    "Description": "Svårt detta",
-    "Tags": ["asd", "aa", "p"],
-    "Comments": [
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Aron", "Comment": "Nä"},]}
-    ]},
-    {"Namn": "Frukost",
-    "Ingredients": [
-      {"Namn": "Yoghurt", "Mängd": "3 dl"},
-      {"Namn": "Flingor", "Mängd": "50 g"},
-    ],
-    "Description": "Häll i Flingor",
-    "Tags": ["asd", "aa", "p"],
-    "Comments": [
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Helena", "Comment": "Ja"},]}
-    ]},
-    {"Namn": "Macka",
-    "Ingredients": [
-      {"Namn": "Bröd", "Mängd": "1 st"},
-      {"Namn": "Smör", "Mängd": "1 paket"},
-    ],
-    "Description": "Bre på smör",
-    "Tags": ["asd", "aa", "p"],
-    "Comments": [
-      {"Poster": "Emil",
-      "Comment": "Det här var ju gott",
-      "Replies": [{"Poster": "Jos-jos", "Comment": "Tycker inte jag"},]}
-    ]},
-  ];
-
 
   return {
 
@@ -87,7 +20,6 @@ angular.module('cookbook.services', [])
       $http({
         method  : 'POST',
         url     : 'http://localhost:8080/api/recipes',
-        // set the headers so angular passing info as form data (not request payload)
         headers : { 'Content-Type': 'application/json' },
         data    :  {
           name: name,
@@ -100,11 +32,11 @@ angular.module('cookbook.services', [])
       }).then(success, error)
     },
 
+    //Adds a comment to recipe with id recipe_id
     addComment: function(recipe_id, name, comment, success, error) {
       $http({
         method  : 'PUT',
         url     : 'http://localhost:8080/api/recipes/comment/' + recipe_id,
-        // set the headers so angular passing info as form data (not request payload)
         headers : { 'Content-Type': 'application/json' },
         data    :  {
           name: name,
@@ -113,11 +45,11 @@ angular.module('cookbook.services', [])
       }).then(success, error);
     },
 
+    //Adds a reply to comment with comment_id to the recipe with id recipe_id
     addReply: function(recipe_id, comment_id, name, reply, success, error) {
       $http({
         method  : 'PUT',
         url     : 'http://localhost:8080/api/recipes/' + recipe_id + '/' +  comment_id,
-        // set the headers so angular passing info as form data (not request payload)
         headers : { 'Content-Type': 'application/json' },
         data    :  {
           name: name,
@@ -126,6 +58,7 @@ angular.module('cookbook.services', [])
       }).then(success, error)
     },
 
+    //Returns requested recipe
     getRecipe: function(id, success, error) {
       $http.get('http://localhost:8080/api/recipes/' + id).then(success, error);
     }
